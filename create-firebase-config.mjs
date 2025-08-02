b7554a581f94900554776df7dc0c1dbae426fca2
Govind Choudhari <choudharygovind553@gmail.com>
2025-08-02 15:18:48 +0530

    Build commit

---
+++ create-firebase-config.mjs
@@ -0,0 +1,20 @@
+import fs from 'fs';
+
+const configFilePath = './firebase-config.js';
+
+// Vercel makes environment variables available in the 'process.env' object.
+const firebaseConfigContent = `
+export const firebaseConfig = {
+    apiKey: "${process.env.PUBLIC_FIREBASE_API_KEY}",
+    authDomain: "${process.env.PUBLIC_FIREBASE_AUTH_DOMAIN}",
+    projectId: "${process.env.PUBLIC_FIREBASE_PROJECT_ID}",
+    storageBucket: "${process.env.PUBLIC_FIREBASE_STORAGE_BUCKET}",
+    messagingSenderId: "${process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID}",
+    appId: "${process.env.PUBLIC_FIREBASE_APP_ID}",
+    measurementId: "${process.env.PUBLIC_FIREBASE_MEASUREMENT_ID}"
+};
+`;
+
+fs.writeFileSync(configFilePath, firebaseConfigContent);
+
+console.log('Successfully created firebase-config.js for Vercel deployment!');
